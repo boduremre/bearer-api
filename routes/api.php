df4471bd3286b6d69d9a78f0bbd13d297dfdfd83
boduremre <emrebodurbto@gmail.com>
2026-05-13 17:26:31 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,13 +14,27 @@ Route::post('/login', [ApiAuthController::class, 'login']);
 
 // Korumalı Rotalar (Bearer Token Gerektirir)
 Route::middleware('auth:sanctum')->group(function () {
+    // Şifre güncelleme rotası    
     Route::post('/update-password', [ApiAuthController::class, 'update_password']);
+
+    // Tek cihazdan çıkış yapma rotası
     Route::post('/logout', [ApiAuthController::class, 'logout']);
+
+    // Tüm cihazlardan çıkış yapma rotası
     Route::post('/logout-all-devices', [ApiAuthController::class, 'logout_all']);
+
+    // CSRF Token Göster
+    Route::get('/csrf-token', function (Request $request) {
+        return ['csrf_token' => $request->cookie('XSRF-TOKEN')];
+    });
 
     // Kullanıcı bilgilerini döndüren rota
     Route::get('/user', function (Request $request) {
-        return $request->user();
+        return [
+            'bearer_token' => $request->bearerToken(),
+            'csrf_token' => $request->cookie('XSRF-TOKEN'),
+            'user' => $request->user()
+        ];
     });
 
     // Tüm post işlemleri için tek satır yeterli (index, store, show, update, destroy)
